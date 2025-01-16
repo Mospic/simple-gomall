9e369b803b163ed5bd148eaca10504dd48631109
mospic <mospic2001@gmail.com>
2025-01-16 14:30:05 +0800

    update .gitignore & user

--- user/model/user.go
+++ user/model/user.go
@@ -7,7 +7,8 @@ import (
 
 type User struct {
 	gorm.Model
-	UserId   int64  `gorm:"primary_key;auto_increment"`
+	UserId   int32  `gorm:"primary_key;auto_increment"`
+	Email    string `gorm:"type:varchar(255);unique"`
 	Name     string `gorm:"default:(-)"`
 	Password string `gorm:"default:(-)"`
 }
@@ -34,7 +35,7 @@ func NewUserDao() *UserDao {
 根据用户名和密码，创建一个新的User，返回UserId
 */
 
-func (*UserDao) CreateUser(user *User) (int64, error) {
+func (*UserDao) CreateUser(user *User) (int32, error) {
 	/*user := User{Name: username, Password: password, FollowingCount: 0, FollowerCount: 0, CreateAt: time.Now()}*/
 
 	result := DB.Create(&user)
@@ -50,10 +51,32 @@ func (*UserDao) CreateUser(user *User) (int64, error) {
 根据用户ID 查找用户实体
 */
 
-func (d *UserDao) FindUserByID(id int64) (*User, error) {
+func (d *UserDao) FindUserByID(id int32) (*User, error) {
 	user := User{UserId: id}
 
 	result := DB.Where("user_id = ?", id).First(&user)
+	err := result.Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, err
+}
+
+func (*UserDao) FindUserByName(username string) (*User, error) {
+	user := User{Name: username}
+
+	result := DB.Where("name = ?", username).First(&user)
+	err := result.Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, err
+}
+
+func (*UserDao) FindUserByEmail(email string) (*User, error) {
+	user := User{Email: email}
+
+	result := DB.Where("email = ?", email).First(&user)
 	err := result.Error
 	if err != nil {
 		return nil, err
