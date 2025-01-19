30937d0afc32874ab76af2ac3446a4357bcbfcd2
serdswe <141556360+serdswe@users.noreply.github.com>
2025-01-19 11:35:01 +0800

    Add files via upload

--- api-gateway/weblib/handlers/user.go
+++ api-gateway/weblib/handlers/user.go
@@ -7,24 +7,21 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 // 用户注册
 func Register(ginCtx *gin.Context) {
-	var userReq user.RegisterReq
+	var userReq user.MallUserRegisterRequest
 	//获取用户名和密码
-	if err := ginCtx.ShouldBindJSON(&userReq); err != nil {
-		fmt.Println(err)
-		ginCtx.JSON(400, gin.H{"error": "Invalid JSON"})
-		return
-	}
+	userReq.Username = ginCtx.Query("username")
+	userReq.Password = ginCtx.Query("password")
 
 	// 从gin.Key中取出服务实例
 	userService := ginCtx.Keys["userService"].(user.UserService)
 
 	//调用user微服务，将context的上下文传入
 	userResp, err := userService.Register(context.Background(), &userReq)
-
 	PanicIfUserError(err)
 
 	//生成token
@@ -33,17 +30,20 @@ func Register(ginCtx *gin.Context) {
 		token, err = utils.GenerateToken(userResp.UserId)
 		PanicIfUserError(err)
 	}
-	token = token // for tmp
+
 	//返回
-	ginCtx.JSON(http.StatusOK, user.RegisterResp{
-		UserId: userResp.UserId,
+	ginCtx.JSON(http.StatusOK, user.MallUserRegisterResponse{
+		StatusCode: userResp.StatusCode,
+		StatusMsg:  userResp.StatusMsg,
+		UserId:     userResp.UserId,
+		Token:      token,
 	})
 }
 
 // 用户登录
 func Login(ginCtx *gin.Context) {
-	var userReq user.LoginReq
-	userReq.Email = ginCtx.Query("username")
+	var userReq user.MallUserLoginRequest
+	userReq.Username = ginCtx.Query("username")
 	userReq.Password = ginCtx.Query("password")
 
 	// 从gin.Key中取出服务实例
@@ -56,36 +56,35 @@ func Login(ginCtx *gin.Context) {
 	if userResp != nil && userResp.UserId > 0 {
 		token, err = utils.GenerateToken(userResp.UserId)
 		PanicIfUserError(err)
-		fmt.Println("登录的token是:" + token)
-
-		ginCtx.JSON(http.StatusOK, user.LoginResp{
-			UserId: userResp.UserId,
-		})
-	} else {
-		ginCtx.JSON(http.StatusOK, user.LoginResp{
-			UserId: -1,
-		})
 	}
 
+	fmt.Println("登录的token是:" + token)
+
+	ginCtx.JSON(http.StatusOK, user.MallUserLoginResponse{
+		StatusCode: userResp.StatusCode,
+		StatusMsg:  userResp.StatusMsg,
+		UserId:     userResp.UserId,
+		Token:      token,
+	})
 }
 
-// // 获取用户的详细信息
+// 获取用户的详细信息
 func UserInfo(ginCtx *gin.Context) {
-	//var userReq user.DouyinUserRequest
-	////将获取到的user_id转换成int类型
-	//user_id, err := strconv.ParseInt(ginCtx.Query("user_id"), 10, 64)
-	//PanicIfUserError(err)
-	//
-	//userReq.UserId = user_id
-	//userReq.Token = ginCtx.Query("token")
-	//
-	//userService := ginCtx.Keys["userService"].(user.UserService)
-	//userResp, err := userService.UserInfo(context.Background(), &userReq)
-	//PanicIfUserError(err)
-	//
-	//ginCtx.JSON(http.StatusOK, user.DouyinUserResponse{
-	//	StatusCode: userResp.StatusCode,
-	//	StatusMsg:  userResp.StatusMsg,
-	//	User:       userResp.User,
-	//})
+	var userReq user.MallUserRequest
+	//将获取到的user_id转换成int类型
+	user_id, err := strconv.ParseInt(ginCtx.Query("user_id"), 10, 64)
+	PanicIfUserError(err)
+
+	userReq.UserId = user_id
+	userReq.Token = ginCtx.Query("token")
+
+	userService := ginCtx.Keys["userService"].(user.UserService)
+	userResp, err := userService.UserInfo(context.Background(), &userReq)
+	PanicIfUserError(err)
+
+	ginCtx.JSON(http.StatusOK, user.MallUserResponse{
+		StatusCode: userResp.StatusCode,
+		StatusMsg:  userResp.StatusMsg,
+		User:       userResp.User,
+	})
 }
