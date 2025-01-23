2059f3bdb4bc229caa6087ac6f3417f5dfc82a17
wlz <1262794380@qq.com>
2025-01-23 19:52:51 +0800

    feat ListProducts, GetProduct, SearchProducts

--- product/core/productService.go
+++ product/core/productService.go
@@ -1,5 +1,92 @@
 package core
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"product/model"
+	services "product/services"
+)
 
+type productService struct {
+}
+
+/*
+* 获取商品信息
+ */
+func (*productService) GetProduct(ctx context.Context, req *services.GetProductReq, resp *services.GetProductResp) error {
+	productId := req.Id
+	product, err := model.NewProductDao().FindProductByID(productId)
+	if err != nil {
+		fmt.Println("获取商品信息失败")
+		return err
+	}
+	// 获取商品分类
+	categoryNameList, err := model.NewProductCategoryDao().FindCategoryNameByProductID(productId)
+	resp.Product = &services.Product{
+		Id:          product.ProductId,
+		Name:        product.Name,
+		Description: product.Description,
+		Picture:     product.Picture,
+		Price:       product.Price,
+		Categories:  categoryNameList,
+	}
+	return nil
+}
+
+/*
+* 分页获取商品列表
+ */
+func (*productService) ListProducts(ctx context.Context, req *services.ListProductsReq, resp *services.ListProductsResp) error {
+	// 获取商品列表
+	productList, err := model.NewProductDao().ListProducts(req.Page, req.PageSize, req.CategoryName)
+	if err != nil {
+		fmt.Println("获取商品列表失败")
+		return err
+	}
+	// 获取商品分类
+	for _, product := range productList {
+		categoryNameList, err := model.NewProductCategoryDao().FindCategoryNameByProductID(product.ProductId)
+		if err != nil {
+			fmt.Println("获取商品分类失败")
+			return err
+		}
+		resp.Products = append(resp.Products, &services.Product{
+			Id:          product.ProductId,
+			Name:        product.Name,
+			Description: product.Description,
+			Picture:     product.Picture,
+			Price:       product.Price,
+			Categories:  categoryNameList,
+		})
+	}
+	return nil
+}
+
+/*
+* 查询获取商品列表
+ */
+func (*productService) SearchProducts(ctx context.Context, req *services.SearchProductsReq, resp *services.SearchProductsResp) error {
+	// 获取商品列表
+	productList, err := model.NewProductDao().SearchProducts(req.Query)
+	if err != nil {
+		fmt.Println("查询商品列表失败")
+		return err
+	}
+	// 获取商品分类
+	for _, product := range productList {
+		categoryNameList, err := model.NewProductCategoryDao().FindCategoryNameByProductID(product.ProductId)
+		if err != nil {
+			fmt.Println("获取商品分类失败")
+			return err
+		}
+		resp.Results = append(resp.Results, &services.Product{
+			Id:          product.ProductId,
+			Name:        product.Name,
+			Description: product.Description,
+			Picture:     product.Picture,
+			Price:       product.Price,
+			Categories:  categoryNameList,
+		})
+	}
+	return nil
 }
--- product/model/product.go
+++ product/model/product.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Product struct {
-	ProductId   int32     `gorm:"primary_key;auto_increment"`
+	ProductId   uint32    `gorm:"primary_key;auto_increment"`
 	Name        string    `gorm:"default:(-):not null"`
 	Description string    `gorm:"default:(-)"`
 	Picture     string    `gorm:"default:(-)"`
@@ -33,4 +33,65 @@ func NewProductDao() *ProductDao {
 			productDao = &ProductDao{}
 		})
 	return productDao
+}
+
+/*
+*
+根据商品ID查找商品实体
+*/
+func (d *ProductDao) FindProductByID(id uint32) (*Product, error) {
+	product := Product{ProductId: id}
+
+	result := DB.Where("product_id = ?", id).First(&product)
+	err := result.Error
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
+
+/*
+* 根据商品ID列表查询商品实体
+ */
+func (d *ProductDao) FindProductByIDs(ids []uint32) ([]*Product, error) {
+	var productList []*Product
+	result := DB.Where("product_id in (?)", ids).Find(&productList)
+	err := result.Error
+	if err != nil {
+		return nil, err
+	}
+	return productList, nil
+}
+
+/*
+* 分页获取商品列表
+ */
+func (d *ProductDao) ListProducts(page int32, pageSize int64, categoryName string) ([]*Product, error) {
+	// 先根据分类名查找商品ID
+	productCategoryDao := NewProductCategoryDao()
+	productIds, err := productCategoryDao.FindProductIDByCategoryName(categoryName)
+	if err != nil {
+		return nil, err
+	}
+	// 再根据商品ID分页获取商品实体
+	var productList []*Product
+	result := DB.Where("product_id in (?)", productIds).Offset(int64(page-1) * pageSize).Limit(pageSize).Find(&productList)
+	err = result.Error
+	if err != nil {
+		return nil, err
+	}
+	return productList, nil
+}
+
+/*
+* 查询获取商品列表
+ */
+func (d *ProductDao) SearchProducts(keyword string) ([]*Product, error) {
+	var productList []*Product
+	result := DB.Where("name like ?", "%"+keyword+"%").Find(&productList)
+	err := result.Error
+	if err != nil {
+		return nil, err
+	}
+	return productList, nil
 }
