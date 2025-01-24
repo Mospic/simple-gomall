fce762b0cd1f19d5b11f50c01ad2a753b6f2f188
614752076 <614752076@qq.com>
2025-01-24 17:55:29 +0800

    Refactor jwt as token util

--- tokenutils/core/tokenService.go
+++ tokenutils/core/tokenService.go
@@ -2,13 +2,8 @@ package core
 
 import (
 	"context"
-	"fmt"
 	"github.com/dgrijalva/jwt-go"
-	redisv8 "github.com/go-redis/redis/v8"
-	"google.golang.org/protobuf/types/known/emptypb"
-	"time"
 	"tokenutils/service"
-	"tokenutils/utils/redis"
 )
 
 type TokenService struct {
@@ -26,33 +21,13 @@ func (*TokenService) GetIdByToken(ctx context.Context, req *service.GetIdByToken
 	token = string(token)
 
 	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (i interface{}, e error) { return jwtSecret, nil })
-	val, err := redis.RdbJwt.Get(ctx, "token").Result()
-	if err == redisv8.Nil {
-		fmt.Println("键不存在")
-		return nil // TODO 自定义 error
-	} else if err != nil {
-		fmt.Println("读取键值对失败: %v", err)
-		return nil
-	}
 
 	if tokenClaims != nil {
-		if claims, ok := tokenClaims.Claims.(*Claims); ok && tokenClaims.Valid && int32(claims.Id) == val {
+		if claims, ok := tokenClaims.Claims.(*Claims); ok && tokenClaims.Valid {
 			out.UserId = int32(claims.Id)
 			return nil
 		}
 	}
 	return err
 
-}
-
-func (*TokenService) StoreTokenToID(ctx context.Context, req *service.StoreTokenToIDRequest, out *emptypb.Empty) error {
-	token := req.UserToken
-	userID := req.UserId
-	token = string(token)
-	// 将 Token 和 userID 存储到 Redis，并设置过期时间
-	err := redis.RdbJwt.Set(context.Background(), token, userID, time.Hour).Err()
-	if err != nil {
-		return err
-	}
-	return nil
 }
